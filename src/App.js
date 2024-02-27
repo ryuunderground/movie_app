5f5c671d863cb5be4b2317b7b21d8a0754503f51
ryuunderground <138040119+ryuunderground@users.noreply.github.com>
2024-02-28 00:32:17 +0900

    img alt

--- src/App.js
+++ src/App.js
@@ -4,46 +4,52 @@ function Vtuber({ name, picture }) {
   return (
     <div>
       <h3>I love {name}</h3>
-      <img src={picture} />
+      <img src={picture} alt={name} />
     </div>
   );
 }
 
 const vILike = [
   {
+    id: 1,
     name: "Ouro Kronii",
     image:
       "https://hololive.hololivepro.com/wp-content/uploads/2020/07/Ouro-Kronii_pr-img_02.png",
   },
   {
+    id: 2,
     name: "NaNashi Mumei",
     image:
       "https://hololive.hololivepro.com/wp-content/uploads/2020/07/Nanashi-Mumei_pr-img_02.png",
   },
   {
+    id: 3,
     name: "Hakos Baelz",
     image:
       "https://hololive.hololivepro.com/wp-content/uploads/2020/07/Hakos-Baelz_pr-img_02.png",
   },
   {
+    id: 4,
     name: "Ceres Fauna",
     image:
       "https://hololive.hololivepro.com/wp-content/uploads/2020/07/Ceres-Fauna_pr-img_02.png",
   },
   {
+    id: 5,
     name: "IRyS",
     image:
       "https://hololive.hololivepro.com/wp-content/uploads/2020/07/IRyS_pr-img_01-1.png",
   },
 ];
 
-function renderVtuber(idol) {
-  console.log(idol);
-  return <Vtuber name={idol.name} picture={idol.image} />;
-}
-
 function App() {
-  return <div>{vILike.map(renderVtuber)}</div>;
+  return (
+    <div>
+      {vILike.map((idol) => (
+        <Vtuber key={idol.id} name={idol.name} picture={idol.image} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
