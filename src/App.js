2456227f93d0254450782ddc6c3d89aa70176b7f
ryuunderground <138040119+ryuunderground@users.noreply.github.com>
2024-02-28 00:45:04 +0900

    proptype

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Vtuber({ name, picture }) {
+function Vtuber({ name, picture, rating }) {
   return (
     <div>
       <h3>I love {name}</h3>
+      <h4>rating {rating}</h4>
       <img src={picture} alt={name} />
     </div>
   );
 }
+
+Vtuber.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
 
 const vILike = [
   {
@@ -15,30 +23,35 @@ const vILike = [
     name: "Ouro Kronii",
     image:
       "https://hololive.hololivepro.com/wp-content/uploads/2020/07/Ouro-Kronii_pr-img_02.png",
+    rating: 4.5,
   },
   {
     id: 2,
     name: "NaNashi Mumei",
     image:
       "https://hololive.hololivepro.com/wp-content/uploads/2020/07/Nanashi-Mumei_pr-img_02.png",
+    rating: 3.9,
   },
   {
     id: 3,
     name: "Hakos Baelz",
     image:
       "https://hololive.hololivepro.com/wp-content/uploads/2020/07/Hakos-Baelz_pr-img_02.png",
+    rating: 4.7,
   },
   {
     id: 4,
     name: "Ceres Fauna",
     image:
       "https://hololive.hololivepro.com/wp-content/uploads/2020/07/Ceres-Fauna_pr-img_02.png",
+    rating: 3.6,
   },
   {
     id: 5,
     name: "IRyS",
     image:
       "https://hololive.hololivepro.com/wp-content/uploads/2020/07/IRyS_pr-img_01-1.png",
+    rating: 4.4,
   },
 ];
 
@@ -46,7 +59,12 @@ function App() {
   return (
     <div>
       {vILike.map((idol) => (
-        <Vtuber key={idol.id} name={idol.name} picture={idol.image} />
+        <Vtuber
+          key={idol.id}
+          name={idol.name}
+          picture={idol.image}
+          rating={idol.rating}
+        />
       ))}
     </div>
   );
