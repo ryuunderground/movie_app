70bd0063796756b33fbe8bf6a37306e35a286886
ryuunderground <138040119+ryuunderground@users.noreply.github.com>
2024-02-28 01:37:54 +0900

    Component life cycle
    1.Render
    2.Mount
    ----------Update------
    1.Render
    2.Update

--- src/App.js
+++ src/App.js
@@ -1,73 +1,34 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-function Vtuber({ name, picture, rating }) {
-  return (
-    <div>
-      <h3>I love {name}</h3>
-      <h4>rating {rating}</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
-
-Vtuber.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
-
-const vILike = [
-  {
-    id: 1,
-    name: "Ouro Kronii",
-    image:
-      "https://hololive.hololivepro.com/wp-content/uploads/2020/07/Ouro-Kronii_pr-img_02.png",
-    rating: 4.5,
-  },
-  {
-    id: 2,
-    name: "NaNashi Mumei",
-    image:
-      "https://hololive.hololivepro.com/wp-content/uploads/2020/07/Nanashi-Mumei_pr-img_02.png",
-    rating: 3.9,
-  },
-  {
-    id: 3,
-    name: "Hakos Baelz",
-    image:
-      "https://hololive.hololivepro.com/wp-content/uploads/2020/07/Hakos-Baelz_pr-img_02.png",
-    rating: 4.7,
-  },
-  {
-    id: 4,
-    name: "Ceres Fauna",
-    image:
-      "https://hololive.hololivepro.com/wp-content/uploads/2020/07/Ceres-Fauna_pr-img_02.png",
-    rating: 3.6,
-  },
-  {
-    id: 5,
-    name: "IRyS",
-    image:
-      "https://hololive.hololivepro.com/wp-content/uploads/2020/07/IRyS_pr-img_01-1.png",
-    rating: 4.4,
-  },
-];
-
-function App() {
-  return (
-    <div>
-      {vILike.map((idol) => (
-        <Vtuber
-          key={idol.id}
-          name={idol.name}
-          picture={idol.image}
-          rating={idol.rating}
-        />
-      ))}
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+  componentDidMount() {
+    console.log("Component rendered");
+  }
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
+  componentWillUnmount() {
+    console.log("Goodbye, cruel world");
+  }
+  render() {
+    console.log("I'm rendering");
+    return (
+      <div>
+        <h1>The number is {this.state.count}.</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
