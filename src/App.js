2077436e34f8b68cd5449f111dba828657926316
ryuunderground <138040119+ryuunderground@users.noreply.github.com>
2024-02-28 00:24:51 +0900

    map #1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import React from "react";
-import Tomato from "./Tomato";
+
+function Vtuber({ name, picture }) {
+  return (
+    <div>
+      <h3>I love {name}</h3>
+      <img src={picture} />
+    </div>
+  );
+}
+
+const vILike = [
+  {
+    name: "Ouro Kronii",
+    image:
+      "https://hololive.hololivepro.com/wp-content/uploads/2020/07/Ouro-Kronii_pr-img_02.png",
+  },
+  {
+    name: "NaNashi Mumei",
+    image:
+      "https://hololive.hololivepro.com/wp-content/uploads/2020/07/Nanashi-Mumei_pr-img_02.png",
+  },
+  {
+    name: "Hakos Baelz",
+    image:
+      "https://hololive.hololivepro.com/wp-content/uploads/2020/07/Hakos-Baelz_pr-img_02.png",
+  },
+  {
+    name: "Ceres Fauna",
+    image:
+      "https://hololive.hololivepro.com/wp-content/uploads/2020/07/Ceres-Fauna_pr-img_02.png",
+  },
+  {
+    name: "IRyS",
+    image:
+      "https://hololive.hololivepro.com/wp-content/uploads/2020/07/IRyS_pr-img_01-1.png",
+  },
+];
 
 function App() {
   return (
     <div>
-      <div>Hello!!!</div>
-      <Tomato />
+      {vILike.map((idol) => (
+        <Vtuber key={idol.name} name={idol.name} picture={idol.image} />
+      ))}
     </div>
   );
 }
