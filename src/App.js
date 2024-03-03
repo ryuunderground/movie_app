7b87773db1bb234ab9c5f677f3cbc478eaacf5fc
ryuunderground <138040119+ryuunderground@users.noreply.github.com>
2024-03-03 15:51:56 +0900

    movie_app end

--- src/App.js
+++ src/App.js
@@ -1,52 +1,23 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
-import "./App.css";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get(
-      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating"
-    );
-    this.setState({ movies, isLoading: false });
-  };
-  componentDidMount() {
-    this.getMovies();
-  }
-
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section class="container">
-        {isLoading ? (
-          <div class="loader">
-            <span class="loader__text">Loading...</span>
-          </div>
-        ) : (
-          movies.map((movie) => (
-            <div class="movies">
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                year={movie.year}
-              />
-            </div>
-          ))
-        )}
-      </section>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <div>
+        <h3>About</h3>
+        <h3>Home</h3>
+      </div>
+      <HashRouter>
+        <Routes>
+          <Route path="/" exact={true} component={Home}></Route>
+          <Route path="/about" exact={true} component={About}></Route>
+        </Routes>
+      </HashRouter>
+    </div>
+  );
 }
 
 export default App;
